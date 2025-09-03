94161ab0ad9c0dd069a56313c131362d951d3d7f
gabbbb457 <gabrielamellizo45@gmail.com>
2025-09-02 21:53:04 -0500

    dfg

--- Un rio en mi ciudad/Assets/Juan/ScriptsJuan/DetectarBasura.cs
+++ Un rio en mi ciudad/Assets/Juan/ScriptsJuan/DetectarBasura.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class DetectarBasura : MonoBehaviour
 {
     GameControllerNivel1 gameC;
+    public AudioSource squashSound;
+
 
     void Start()
     {
         gameC = FindObjectOfType<GameControllerNivel1>();
+
+
+        if( squashSound == null)
+        {
+            squashSound = GetComponent<AudioSource>();
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,6 +24,11 @@ public class DetectarBasura : MonoBehaviour
         {
             Debug.Log("Basura detectada, perdiendo vida...");
             gameC.PerderVida();
+
+            if (squashSound != null && squashSound.clip != null)
+            {
+                squashSound.PlayOneShot(squashSound.clip);
+            }
 
             // opcional: destruir la basura al tocar el borde
             Destroy(other.gameObject);
